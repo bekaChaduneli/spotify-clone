b132f8fa659e3fd5bbcd70ab77f354dbcdf62d6d
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-12 14:02:42 +0400

    add simple bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Figtree } from "next/font/google";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
@@ -11,31 +13,33 @@ import Player from "@/components/Player";
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Spotify Clone",
-  description: "Listen to music!",
+    title: "Spotify Clone",
+    description: "Listen to music!",
 };
 
 export const revalidate = 0;
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  const userSongs = await getSongsByUserid();
+    const userSongs = await getSongsByUserid();
 
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ToasterProvider />
-        <SupabaseProvider>
-          <UserProvider>
-            <ModalProvider />
-            <Sidebar songs={userSongs}>{children}</Sidebar>
-            <Player />
-          </UserProvider>
-        </SupabaseProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <SimpleBar style={{ maxHeight: 500 }}>
+                    <ToasterProvider />
+                    <SupabaseProvider>
+                        <UserProvider>
+                            <ModalProvider />
+                            <Sidebar songs={userSongs}>{children}</Sidebar>
+                            <Player />
+                        </UserProvider>
+                    </SupabaseProvider>
+                </SimpleBar>
+            </body>
+        </html>
+    );
 }
